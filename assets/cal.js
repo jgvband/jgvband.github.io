2834b6976c5f27c1988bbd174eb95e029b35f02a
Aulternative Media <aulternativemedia@gmail.com>
2026-04-18 12:41:24 -0700

    Update cal.js

--- assets/cal.js
+++ assets/cal.js
@@ -25,7 +25,7 @@ function getMapDirections(location) {
 }
 
 function getEvents(el, id) {
-	var id = id || `c_9f5ddadfef3ca6cdb4677bf1617ffdc84f7aadb24ebe264195e8c0a1155050ad@group.calendar.google.com`;
+	if (id == "") { return; }
 	let key = `AIzaSyCDoa1zVRXmMfdfnDHXkEy_3MLHJUSfrks`;
 	let RFC3339 = new Date().toISOString();
 
@@ -39,16 +39,20 @@ function getEvents(el, id) {
 	}).then(data => {
 		let events = {};
 		data.items.forEach((event) => {
-			start = new Date(event.start.dateTime).toLocaleTimeString('en-US', { hour: 'numeric', minute: '2-digit' });
-			end = new Date(event.end.dateTime).toLocaleTimeString('en-US', { hour: 'numeric', minute: '2-digit' });
+			const dayKey = event.start.dateTime.slice(0, 10); // "YYYY-MM-DD"
 			where = event.location.split(",").map(s => s.trim());
-			events[event.start.dateTime.slice(0, 10)] = {
+
+			if (!events[dayKey]) {
+				events[dayKey] = [];
+			}
+
+			events[dayKey].push({
 				name: event.summary,
 				location: event.location,
 				address: where.slice(1).join(", ").replace(", ", "<br/>"),
-				start: start,
-				end: end
-			};
+				start: event.start.dateTime,
+				end: event.end.dateTime
+			});
 		});
 		upcoming(el, events)
 	}).catch(error => {
@@ -56,10 +60,7 @@ function getEvents(el, id) {
 	});
 }
 
-function upcoming(el, events) {
-	console.log('Events:', events);
-	if (Object.keys(events).length == 0) { return; }
-
+function month(el, events) {
 	const container = document.getElementById(el);
 	const today = new Date();
 
@@ -122,30 +123,119 @@ function upcoming(el, events) {
 
 		// Click handler for dialog
 		cell.addEventListener("click", () => {
-			if (cell.data && cell.data.name) {
-				let when = prettyPrint(cell.data.start);
-				dialogBody.innerHTML = `
-				<h3 class="modal-name">${cell.data.name}</h3>
-				<p class="modal-date">${when} &bull; <span class="time">${cell.data.start} &ndash; ${cell.data.end}</span></p>
-				`;
-				let a = document.createElement('a');
-				a.className = 'modal-addr';
-				a.title = `Get Directions to ${cell.data.location.split(",")[0]}`;
-				a.href = "javascript:void('getDirections');";
-				a.innerHTML = '';
-				a.innerHTML += `<span>${cell.data.location.split(",")[0]}<br/>${cell.data.address}</span>`;
-				a.innerHTML += `<i class="fa-solid fa-diamond-turn-right"></i>`;
-				a.addEventListener("click", function() {
-					getMapDirections(cell.data.location);
-				});
+			dialogBody.innerHTML = '';
+			console.log(cell.data);
+			if (cell.data && cell.data.length > 0) {
+				for (const event of cell.data) {
+					if (event.name) {
+						let when = prettyPrint(event.start);
+						let start = new Date(event.start).toLocaleTimeString('en-US', { hour: 'numeric', minute: '2-digit' });
+						let end = new Date(event.end).toLocaleTimeString('en-US', { hour: 'numeric', minute: '2-digit' });
 
-				dialogBody.appendChild(a);
+						let div = document.createElement('div');
+						div.innerHTML += `
+						<h3 class="modal-name">${event.name}</h3>
+						<p class="modal-date">${when} &bull; <span class="time">${start} &ndash; ${end}</span></p>
+						`;
+						let a = document.createElement('a');
+						a.className = 'modal-addr';
+						a.title = `Get Directions to ${event.location.split(",")[0]}`;
+						a.href = "javascript:void('getDirections');";
+						a.innerHTML = '';
+						a.innerHTML += `<span>${event.location.split(",")[0]}<br/>${event.address}</span>`;
+						a.innerHTML += `<i class="fa-solid fa-diamond-turn-right"></i>`;
+						a.addEventListener("click", function() {
+							getMapDirections(event.location);
+						});
 
-				dialog.showModal();
-				dialog.focus();
+						div.appendChild(a);
+						dialogBody.appendChild(div);
+
+						dialog.showModal();
+						dialog.focus();
+					}
+				}
 			}
 		});
 
 		grid.appendChild(cell);
+	}
+}
+
+function list(el, events, max=3) {
+	try {
+		el = document.getElementById(el) || document.getElementById('event-list');
+		el.id = 'event-list';
+		el.innerHTML = '';
+		let i = 1, max = calendar.limit;
+		const today = new Date();
+
+		for (const date in events) {
+			for (const event of events[date]) {
+				const start = new Date(event.start);
+				if (start < today) { max++; continue; }
+				if (i >= max) { break; }
+
+				const month = start.toLocaleString("en-US", { month: "short" }).toUpperCase();
+				const day   = String(start.getDate()).padStart(2, "0");
+
+				const starttime = start.toLocaleString("en-US", {
+					hour: "numeric",
+					minute: "2-digit",
+					hour12: true
+				});
+
+				const endtime = new Date(event.end).toLocaleString("en-US", {
+					hour: "numeric",
+					minute: "2-digit",
+					hour12: true
+				});
+
+				let cal = document.createElement('span');
+				cal.className = 'date';
+				cal.innerHTML = `<span class="month">${month}</span><span class="day">${day}</span>`;
+
+				let time = document.createElement('span');
+				time.className = 'time';
+				time.innerHTML = `<span class="start">${starttime}</span><span class="end">${endtime}</span>`;
+
+				let name = document.createElement('span');
+				name.className = 'name';
+				name.innerHTML = event.location.split(",")[0];
+
+				let arrow = document.createElement('span');
+				arrow.className = 'arrow';
+				arrow.innerHTML = '<i class="fa-solid fa-diamond-turn-right"></i>';
+
+				let ev = document.createElement('a');
+				ev.className = 'event get-directions';
+				ev.title = `Get Directions to ${event.location.split(",")[0]}`;
+				ev.href = "javascript:void('getDirections');";
+				ev.addEventListener("click", function() {
+					getMapDirections(event.location);
+				});
+
+				ev.appendChild(cal);
+				ev.appendChild(time);
+				ev.appendChild(name);
+				ev.appendChild(arrow);
+
+				el.appendChild(ev);
+
+				i++;
+			}
+		}
+	} catch (error) {
+		console.error("Error while iterating object:", error);
+	}
+}
+
+function upcoming(el, events) {
+	if (Object.keys(events).length == 0) { return; }
+
+	if (calendar.style == "list") {
+		list(el, events);
+	} else {
+		month(el, events);
 	}
 }
